f7e2ed3056aac9da99f80deb6270db02e8cfbf31
yoavbarzur <barzur.yoav@gmail.com>
2021-04-01 14:31:42 +0300

    Adding basic react template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+export const App = () => {
+    const x = '333';
+    return (
+        <Fragment>
+           <h1>
+               fasdfd
+           </h1>
+        </Fragment>
+    );
+}
+
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import './global.scss';
+import ReactDOM from 'react-dom';
+import {App} from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
