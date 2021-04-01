64f08f9fe2fe5a6d5a9d478232345cfe40b03b3c
yoavbarzur <barzur.yoav@gmail.com>
2021-04-01 18:41:56 +0300

    adding fixes for scss and storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -5,6 +7,7 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
 }
